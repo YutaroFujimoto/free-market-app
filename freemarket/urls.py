379611cf85d4cad52e4396d5af659e27ffeeae12
Yutaro Fujimoto <s1F102002479@iniad.org>
2020-12-18 13:55:19 +0900

    買う関係のテンプレート作成
    買う関係のテンプレートを5つ作成しました。

--- freemarket/urls.py
+++ freemarket/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('product_list', views.product_list, name='product_list'),
+    path('product_detail', views.product_detail, name='product_detail'),
+    path('confirm', views.confirm, name='confirm'),
+    path('contact', views.contact, name='contact'),
+    path('seller_information', views.seller_information, name='seller_information'),
 ]
