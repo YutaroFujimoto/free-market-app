7e2147436c410a59700b73039491a870c29fddc4
Takuya Kawase <s1f102002705@iniad.org>
2020-12-18 14:15:29 +0900

    Update urls.py

--- freemarket/urls.py
+++ freemarket/urls.py
@@ -3,10 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
     path('exhibit_finished', views.exhibit_finished, name='exhibit_finished'),
     path('sell_page', views.sell_page, name='sell_page'),
-
     path('login', views.login, name='login'),
     path('information', views.information, name='information'),
     path('change_information', views.change_information, name='change_information'),
