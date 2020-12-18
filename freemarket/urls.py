91875a42444c10c38cccc8963d018b4eabe585e2
Daichi Kato <s1F102002165@iniad.org>
2020-12-18 13:54:34 +0900

    sell

--- freemarket/urls.py
+++ freemarket/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('exhibit_finished', views.exhibit_finished, name='exhibit_finished')
+    path('sell_page', views.sell_page, name='sell_page')
 ]
