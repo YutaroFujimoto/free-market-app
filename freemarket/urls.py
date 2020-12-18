e1abed0db729c58eb7644483a486414d2c2e93cc
Takuya Kawase <s1f102002705@iniad.org>
2020-12-18 13:54:39 +0900

    ユーザー情報

--- freemarket/urls.py
+++ freemarket/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('information', views.information, name='information'),
+    path('change_information', views.change_information, name='change_information'),
 ]
