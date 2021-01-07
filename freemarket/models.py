e02cac32fed71732e6ee12b4e9f0931d02e606a0
Daichi Kato <s1F102002165@iniad.org>
2021-01-08 00:35:11 +0900

    null=True

--- freemarket/models.py
+++ freemarket/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 # 商品のテーブル
 class Product(models.Model):
-    name = models.CharField(max_length=200)
-    genre = models.CharField(max_length=200)
+    name = models.CharField(null = True, max_length=200)
+    genre = models.CharField(null = True, max_length=200)
     explanation = models.TextField()
-    condition = models.CharField(max_length=200)
+    condition = models.CharField(null = True, max_length=200)
     price = models.IntegerField()
-    shipping_cost = models.CharField(max_length=200)
+    shipping_cost = models.CharField(null = True, max_length=200)
     picture_1 = models.ImageField(upload_to='images/')
     picture_2 = models.ImageField(upload_to='images/')
     picture_3 = models.ImageField(upload_to='images/')
